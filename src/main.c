2d3fc9de77569792a593e1115ac3ae830d4e502a
ashtonㅤ <121163457+ashtonjamesd@users.noreply.github.com>
2025-10-21 08:53:56 +0100

    add sample app for development testing

--- src/main.c
+++ src/main.c
@@ -1,30 +1,48 @@
 #include "include/lavandula.h"
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("usage: lavu <command> [options]\n");
-        return 1;
-    }
+appRoute(a, ctx) {
+    return ok("This is route A", TEXT_PLAIN);
+}
 
-    char *option = argv[1];
+appRoute(b, ctx) {
+    return ok("This is route B", TEXT_PLAIN);
+}
 
-    if (strcmp(option, "new") == 0) {
-        if (argc < 3) {
-            printf("error: expected project name after 'new'\n");
-            return 1;
-        }
+int main() {
+    AppBuilder builder = createBuilder();
+    App app = build(builder);
 
-        char *projectName = argv[2];
-        return newProject(projectName);
-    } else if (strcmp(option, "run") == 0) {
-        return runProject();
-    } else if (strcmp(option, "build") == 0) {
-        return buildProject();
-    } else if (strcmp(option, "help") == 0) {
-        return help();
-    } else if (strcmp(option, "--version") == 0 || strcmp(option, "-v") == 0) {
-        return version();
-    } else {
-        return unknownCommand(option);
-    }
+    get(&app, "/a", a);
+    get(&app, "/b", b);
+
+    runApp(&app);
+
+    return 0;
+
+    // if (argc < 2) {
+    //     printf("usage: lavu <command> [options]\n");
+    //     return 1;
+    // }
+
+    // char *option = argv[1];
+
+    // if (strcmp(option, "new") == 0) {
+    //     if (argc < 3) {
+    //         printf("error: expected project name after 'new'\n");
+    //         return 1;
+    //     }
+
+    //     char *projectName = argv[2];
+    //     return newProject(projectName);
+    // } else if (strcmp(option, "run") == 0) {
+    //     return runProject();
+    // } else if (strcmp(option, "build") == 0) {
+    //     return buildProject();
+    // } else if (strcmp(option, "help") == 0) {
+    //     return help();
+    // } else if (strcmp(option, "--version") == 0 || strcmp(option, "-v") == 0) {
+    //     return version();
+    // } else {
+    //     return unknownCommand(option);
+    // }
 }
